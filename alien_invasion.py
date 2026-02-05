587cc05fb99328ac8c97aa7fdce0aaf8165e6ec3
mikitazychar <mikitazychar@gmail.com>
2026-02-05 19:38:08 +0300

    Создан файл с основной логикой игры. Созданы модули настроек игры (экран и цвет фона). Создан модуль корабля, управляемого игроком.
    В осноном файле реализованы:
        - создание экрана игры
        - создание оъекта корабля
    
    Также был произведён рефакторинг класса игры.
    Упрощен метод run_game().
    его функционал перемещён в отдельные вспомогателные методы.

---
+++ alien_invasion.py
@@ -0,0 +1,43 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры."""
+    def __init__(self):
+        """Инициализирует игру и создаёт игровые ресурсы."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
+        pygame.display.set_caption("AlienInvasion")
+        self.ship = Ship(self.screen)
+
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображение на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            self._check_events()
+            self._update_screen()
+
+
+if __name__ == '__main__':
+    # Создание экземпляра и запуск игры.
+    ai = AlienInvasion()
+    ai.run_game()
